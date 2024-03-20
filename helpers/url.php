a47cf52949bddec9ce2cf15e1da39386a3b85ab0
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2024-03-20 14:23:37 -0300

    Update url.php

--- helpers/url.php
+++ helpers/url.php
@@ -1,2 +1,2 @@
 <?php
-$BASE_URL = "http://".$_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']. '?') . '/';
+$BASE_URL = "http://".$_SERVER['SERVER_NAME'] . ":8081" . dirname($_SERVER['REQUEST_URI']. '?') . '/';
