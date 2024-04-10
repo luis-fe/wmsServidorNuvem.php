27e7394d9bc2c262b3b06a959c455f7e0228b677
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2024-04-10 14:16:54 -0300

    Update TelaInicial.php

--- TelaInicial.php
+++ TelaInicial.php
@@ -6,47 +6,117 @@ include_once("./templates/headers.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/TelaInicial.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <script src="https://cdn.jsdelivr.net/npm/apexcharts@3.29.0/dist/apexcharts.min.js"></script>
 </head>
 <style>
-    .Corpo{
-        display: flex;
-        width: 100%;
-        height: 80.5vh;
-    }
+    * {
+    font-family: Arial, sans-serif;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
 
-    .left{
-        width: 45%;
-        height: 100%;
-    }
+:root {
+    --cor1: rgb(153, 153, 153);
+    --cor2: rgb(17, 45, 126);
+    --cor3: rgb(93, 140, 233);
+    --cor4: rgb(210, 210, 210);
+    --cor5: rgb(0, 0, 0);
+}
 
-    .right{
-        display: flex;
-        flex-direction: column;
-        width: 55%;
-        height: 100%;
-    }
 
-    .top{
-        overflow: auto;
-            border-radius: 10px;
+.container {
+            padding: 20px;
             width: 100%;
-            height: 50%;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            height: 90vh;
+            display: block;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
 
-    }
+.title{
+    display: flex;
+    border-bottom: 2px solid var(--cor2);
+    padding: 20px;
+    background-color: var(--cor4);
+}
 
-    .bottom{
-        overflow: auto;
-            border-radius: 10px;
+.title i {
+    flex-shrink: 0;
+    font-size: 25px;
+    cursor: pointer;
+    color: black;
+}
+
+.title h3 {
+    margin: 0;
+    text-align: left;
+    flex-grow: 1;  
+    font-size: 25px; 
+}
+
+
+
+.Datas input[type="date"]{
+    margin-left: 10px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    padding: 10px;
+    border-top: none;
+    border-right: none;
+    border-left: none;
+    background-color: transparent;
+    border-bottom: 2px solid black;
+}
+
+.Datas button{
+    padding: 10px;
+    border-radius: 10px;
+    background-color: var(--cor2);
+    color: white;
+    cursor: pointer;
+}
+.Corpo {
+            display: flex;
+            justify-content: space-between;
+            align-items: stretch;
             width: 100%;
-            height: 50%;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
+            flex-grow: 1;
+            overflow-x: auto;
+            max-height: 70vh;
+        }
 
+        .left, .right {
+            width: calc(50% - 20px);
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-bottom: 20px;
+            max-height: 100%;
+            overflow-y: auto;
+        }
+
+        .chart-container {
+            min-height: 300px;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            padding: 20px;
+            position: relative;
+        }
+
+        .loading-icon {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 24px;
+            color: #245580;
+            display: none;
+        }
 </style>
 <body>
     <div class="container">
@@ -72,7 +142,10 @@ include_once("./templates/headers.php");
                 <h2>Ranking Defeitos</h2>
                 <div id="chart"></div>
                 </div>
-                <div class="bottom"></div>
+                <div class="bottom">
+                <h2>Ranking Defeitos - FACCIONISTA / FORNECEDOR</h2>
+                <div id="chartFaccionista"></div>
+                </div>
             </div>
         </div>
     </div>
@@ -80,6 +153,7 @@ include_once("./templates/headers.php");
 <script>
         const ApiMotivos = 'http://192.168.0.183:5000/api/MotivosAgrupado';
         const ApiQualidade = 'http://192.168.0.183:5000/api/AcompanhamentoQualidade';
+        const ApiTerceirizados = 'http://192.168.0.183:5000/api/OrigemAgrupado';
 
         async function fetchData(apiUrl, dataInicial, dataFinal) {
             try {
@@ -102,11 +176,34 @@ include_once("./templates/headers.php");
             }
         }
 
+        async function fetchData1(apiUrl, dataInicial, dataFinal) {
+            try {
+                const response = await fetch(`${apiUrl}?DataIncial=${dataInicial}&DataFinal=${dataFinal}&origem=COSTURA`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': "a40016aabcx9"
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error('Erro no retorno da API');
+                }
+
+                return response.json();
+            } catch (error) {
+                console.error(error);
+                return null;
+            }
+        }
+
         async function renderBarChart(data) {
+            const chartHeight = Math.max(300, data.length * 50); // Calcula a altura com base na quantidade de dados
+
             const chartOptions = {
                 chart: {
                     type: 'bar',
-                    height: '195%',
+                    height: `${chartHeight}px`,
                     width: '100%',
                 },
                 series: [{
@@ -133,6 +230,38 @@ include_once("./templates/headers.php");
             chart.render();
         }
 
+        async function renderBarChartTerceirizados(data) {
+            const chartHeight = Math.max(300, data.length * 50); // Calcula a altura com base na quantidade de dados
+            const chartOptions = {
+                chart: {
+                    type: 'bar',
+                    height: `${chartHeight}px`,
+                    width: '100%',
+                },
+                series: [{
+                    name: 'Quantidade',
+                    data: data.map(item => item.qtde)
+                }],
+                xaxis: {
+                    categories: data.map(item => item.Origem),
+                    labels: {
+                        rotate: -90,
+                        align: 'center',
+                    }
+                },
+                plotOptions: {
+                    bar: {
+                        borderRadius: 4,
+                        barHeight: 80,
+                        horizontal: true,
+                    }
+                }
+            };
+
+            const chart = new ApexCharts(document.querySelector("#chartFaccionista"), chartOptions);
+            chart.render();
+        }
+
         async function renderDonutChart(data) {
     if (data.length > 0) {
         const totalPecas = parseFloat(data[0]["2- Total Peças Baixadas periodo"]);
@@ -145,7 +274,7 @@ include_once("./templates/headers.php");
             const chartOptions = {
                 chart: {
                     type: 'donut',
-                    width: '100%',
+                    width: '80%',
                 },
                 series: [porcentagem2Qualidade, porcentagemDiferenca],
                 labels: ['Peças 2ª Qualidade', 'Peças 1ª Qualidade'],
@@ -154,7 +283,7 @@ include_once("./templates/headers.php");
                     breakpoint: 480,
                     options: {
                         chart: {
-                            width: 200
+                            width: 200,
                         },
                         legend: {
                             position: 'bottom'
@@ -227,11 +356,12 @@ async function initializeCharts() {
     // Limpa o conteúdo dos elementos onde os gráficos serão renderizados
     document.getElementById("chart").innerHTML = '';
     document.getElementById("chartQualidade").innerHTML = '';
+    document.getElementById("chartFaccionista").innerHTML = '';
 
     const dataInicial = document.getElementById("dataInicial").value.split("-").reverse().join("/");
     const dataFinal = document.getElementById("dataFinal").value.split("-").reverse().join("/");
 
-    const dataMotivos = await fetchData(ApiMotivos, dataInicial, dataFinal);
+    const dataMotivos = await fetchData1(ApiMotivos, dataInicial, dataFinal);
     if (dataMotivos) {
         renderBarChart(dataMotivos);
     }
@@ -240,7 +370,16 @@ async function initializeCharts() {
     if (dataQualidade) {
         renderDonutChart(dataQualidade);
     }
+
+
+    const dataTerceirizados = await fetchData1(ApiTerceirizados, dataInicial, dataFinal);
+    if (dataTerceirizados) {
+        renderBarChartTerceirizados(dataTerceirizados);
+    }
+
+    
 }
+
 
 document.addEventListener("DOMContentLoaded", function() {
     const today = new Date();
