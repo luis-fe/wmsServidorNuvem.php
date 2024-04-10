0159e2645c2b5da90a1cebf54951bb14aa0114b3
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2024-04-10 14:32:45 -0300

    Update DashboardProdutividade.php

--- DashboardProdutividade.php
+++ DashboardProdutividade.php
@@ -12,6 +12,7 @@ include_once("./templates/headers.php");
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
+    
 </head>
 <body>
 <div class="container">
